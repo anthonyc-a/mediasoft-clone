00f8b83fa891816eb0d345ee2393e3c03de556c3
anthonyc-a <anthonyc.animba@gmail.com>
2021-12-20 00:45:59 +0000

    -

--- src/components/Content/Banner/Banner.jsx
+++ src/components/Content/Banner/Banner.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Banner = () => {
     return (
         <section className='banner section'>
-            
+            <div className="container">
+                <div className="banner-group">
+                    
+                </div>
+            </div>
         </section>
     )
 }
